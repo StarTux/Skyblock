c044695e9fdcb42b7557416c2e4f5ae9989f5c3c
StarTux <ths.loy@gmail.com>
2025-07-01 20:39:08 +0200

    Fix the list view

--- src/main/java/com/cavetale/skyblock/WorldChoiceGui.java
+++ src/main/java/com/cavetale/skyblock/WorldChoiceGui.java
@@ -18,6 +18,7 @@ import org.bukkit.inventory.ItemStack;
 import static com.cavetale.skyblock.SkyblockPlugin.plugin;
 import static io.papermc.paper.datacomponent.item.TooltipDisplay.tooltipDisplay;
 import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.Component.textOfChildren;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 @Getter
@@ -27,7 +28,7 @@ public final class WorldChoiceGui {
     private BuildWorld buildWorld;
     private final List<BuildWorld> availableWorlds = new ArrayList<>();
     private boolean importsLoaded;
-    private int listOffset = 0;
+    private int page = 0;
 
     public WorldChoiceGui enable() {
         buildWorld = BuildWorld.findWithPath("skyblock");
@@ -44,13 +45,21 @@ public final class WorldChoiceGui {
         }
         Gui gui = new Gui(plugin())
             .size(6 * 9)
-            .layer(GuiOverlay.BLANK, BLUE)
-            .title(text("Choose a World", WHITE));
+            .layer(GuiOverlay.BLANK, BLUE);
         // Skyblock worlds
+        final int pageSize = 8;
+        final int listOffset = page * pageSize;
+        final int maxPage = (availableWorlds.size() - 1) / pageSize;
+        if (maxPage > 0) {
+            gui.title(textOfChildren(text("Choose a World ", WHITE),
+                                     text("(" + (page + 1) + "/" + (maxPage + 1) + ")", GRAY)));
+        } else {
+            gui.title(text("Choose a World", WHITE));
+        }
         for (int i = 0; i < 8; i += 1) {
             final int availableWorldsIndex = i + listOffset;
             if (availableWorldsIndex >= availableWorlds.size()) break;
-            final int column = i / 4;
+            final int column = (i / 4) * 3;
             final int row = i % 4;
             final BuildWorld theBuildWorld = availableWorlds.get(availableWorldsIndex);
             final ItemStack icon;
@@ -71,19 +80,19 @@ public final class WorldChoiceGui {
                     open();
                 });
         }
-        if (listOffset > 0) {
+        if (page > 0) {
             gui.setItem(0, 0, Mytems.ARROW_LEFT.createIcon(List.of(text("Previous Page", GRAY))), click -> {
                     if (!click.isLeftClick()) return;
                     player.playSound(player, Sound.UI_BUTTON_CLICK, 1f, 1f);
-                    listOffset -= 8;
+                    page -= 1;
                     open();
                 });
         }
-        if (listOffset < availableWorlds.size() / 8) {
+        if (page < maxPage) {
             gui.setItem(8, 0, Mytems.ARROW_RIGHT.createIcon(List.of(text("Next Page", GRAY))), click -> {
                     if (!click.isLeftClick()) return;
                     player.playSound(player, Sound.UI_BUTTON_CLICK, 1f, 1f);
-                    listOffset += 8;
+                    page += 1;
                     open();
                 });
         }
